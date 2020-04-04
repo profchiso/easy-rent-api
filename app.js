28cf0f34de2c42316add0ef52ca185d5a16b4e63
Chinedu Okorie <chinedu.okorie@pmglobaltechnology.com>
2020-04-05 00:09:44 +0100

    add middlewares for api security

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitizer = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 //user defined modules
 const connectToDB = require('./controllers/dbController');
@@ -10,15 +15,38 @@ const appartmentRoute = require('./routes/api/appartment');
 const authRoute = require('./routes/api/auth');
 const undefinedRoutes = require('./routes/api/404-routes');
 
-connectToDB();
+connectToDB(); //function to connect to database
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-//console.log(parseInt('2280873', 8));
+
 //middlewares
-app.use(express.json({ extended: false }));
-app.use(cors());
+//middleware to set security HTTP headers
+app.use(helmet());
+
+//middleware  to limit the number of request per hour from any IP address
+const limiter = rateLimit({
+	max: 300, //max no of request per IP in the specified time
+	windowMs: 60 * 60 * 1000, //time allowed for the num of request
+	message:
+		'Maximum allowed request  for an IP in an hour exceeded, please try again in an hour time' //
+});
+app.use('/easy-rent/api/', limiter); //sends statusCode 429 which means too many request when limit is exceeded, always used to curb brute-force attack
+app.use(express.json({ extended: false })); //middleware for body-paser
+app.use(cors()); //middle ware to allow cross origin  request
+
+//protect DB from NOSQL query injections using the express-mongo-sanitize middleware
+// interset the req.body, req.params, and req.query
+app.use(mongoSanitizer());
+
+//protect data from xss attack using the xss-clean middleware
+// work on HTML to sanitize the data from malicious script
+app.use(xss());
+
+//protect against parameter pollution using the hpp middleware
+//works on url parameters to sanitize it eg. remove duplicate parameters
+app.use(hpp({ whitelist: [] })); // use the whitelist option to specify some parameter that you want to allow duplicate in the array
 
 //routes
 app.use('/easy-rent/api/v1/users', userRoute);
